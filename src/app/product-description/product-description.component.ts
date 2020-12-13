ff46390da0f7fd35b6816e59493d7c0eaf2286de
Eduardo Manuel Santos <>
2020-12-13 18:38:16 +0000

    Completed module

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService  } from '../product.service';
+
+import { ProductService } from '../product.service';
+
 @Component({
   selector: 'app-product-description',
   templateUrl: './product-description.component.html',
@@ -7,8 +9,10 @@ import { ProductService  } from '../product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  constructor(private _productService:ProductService) { }
   albumInfo;
+
+  constructor(private _productService: ProductService) { }
+
   ngOnInit() {
     this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
   }
