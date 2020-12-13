0ea05d47988bbe81c41d7f0618ae3811c9e4bc4c
Eduardo Manuel Santos <>
2020-12-13 18:04:00 +0000

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/Http';
-import { Response } from '@angular/Http';
+import { Http } from '@angular/http';
+import { Response } from '@angular/http';
 
 @Injectable()
 export class ProductService {
